8c525bced8c8ad6b09e6144bffa1e48495889fd0
DESKTOP-LL1J0VE\isd.hcm <duyhungsky02@gmail.com>
2025-06-21 15:04:11 +0700

    update

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const { pool } = require('./database/db'); // Sử dụng MySQL pool thay vì SQLite
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const multer = require('multer');
 const cors = require('cors');
 require('dotenv').config();
